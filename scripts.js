311531fa242a8e4143d4004a0c2c189d413cd967
ryant4 <trekell.dev@gmail.com>
2022-10-02 21:16:12 -0400

    Complete project with functioning buttons and select element

--- scripts.js
+++ scripts.js
@@ -3,39 +3,40 @@ const sketchContainer = document.querySelector('#sketch-container');
 
 function buildGrid () {
 
-    let height = 40;
-    let width = 40;
+    let gridSize = document.getElementById('setting-style')
+    let height = gridSize.value;
+    let width = height;
 
-for (let i = 0; i < height; i++) {
+    for (let i = 0; i < height; i++) {
 
     const newLine = document.createElement('div')
     newLine.setAttribute('class', 'container');
 
-    for (let j = 0; j < width; j++) {
-        const newDiv = document.createElement('div');
-        newDiv.setAttribute('class', 'etch-pixel');
-        newDiv.addEventListener('mouseenter', () => {
-            newDiv.setAttribute('class', 'marked-pixel');
-        })
-        newLine.appendChild(newDiv);
-        
-
-    }
-
+        for (let j = 0; j < width; j++) {
+            const newDiv = document.createElement('div');
+            newDiv.setAttribute('class', 'etch-pixel');
+            newDiv.addEventListener('mouseenter', () => {
+                newDiv.setAttribute('class', 'marked-pixel');
+            })
+            newLine.appendChild(newDiv);
+        }
     sketchContainer.appendChild(newLine);
-
 }
 }
 
-function eraseGrid () {
+function newGrid () {
     while(sketchContainer.firstChild) {
         sketchContainer.removeChild(sketchContainer.firstChild);
     }
-
+    buildGrid();
 }
 
-buildGrid();
 
-eraseGrid();
 
 buildGrid();
+
+const clearButton = document.querySelector('#button-style');
+clearButton.addEventListener('click', newGrid);
+
+const sizeSelector = document.querySelector('#setting-style');
+sizeSelector.addEventListener('change', newGrid);
